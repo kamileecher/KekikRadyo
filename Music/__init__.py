df4f15bc47904f403c0344f4db1daecf116e1fee
keyiflerolsun <keyiflerolsun@gmail.com>
2021-07-07 23:39:42 +0300

    🕊  BB ARQ

--- Music/__init__.py
+++ Music/__init__.py
@@ -2,7 +2,5 @@
 
 from Music.get_telegram import telegram
 from Music.get_youtube  import youtube
-from Music.get_deezer   import deezer
-from Music.get_saavn    import saavn
 
-available_services = {"telegram":telegram, "youtube":youtube, "deezer":deezer, "saavn":saavn}
+available_services = {"telegram":telegram, "youtube":youtube}
